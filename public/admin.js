0d59e92b3982273ced8d686663bb6e1ff26e2d5c
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-04-12 21:16:15 -0500

    updated

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,41 @@
+async function main(){
+    let list = await getBookList()
+    let root = document.getElementById('root')
 
-// Your Code Here
+    list.forEach(element => {
+        let line = document.createElement('p') 
+        line.textContent = element.title
+        root.appendChild(line)
+
+        let form = document.createElement('form')
+        let input = document.createElement('input')
+        let button = document.createElement('input')
+
+        input.defaultValue = element.quantity
+        form.appendChild(input)
+
+        button.type = "submit"
+        button.value = "Save"
+        button.setAttribute('id', element.id)
+        button.addEventListener('click', () => {
+            
+    fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                }, body: JSON.stringify({ id: element.id, quantity: input.value,
+                }),
+            });
+        })
+        form.appendChild(button)
+        root.appendChild(form)
+    });
+}
+
+async function getBookList(){
+    let response = await fetch('http://localhost:3001/listBooks')
+    let bookList = await response.json()
+    return bookList
+}
+
+main()
